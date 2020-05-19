41fe121e8b189939dc7e047d519f183aa3f5ae07
ggraur <39068501+ggraur@users.noreply.github.com>
2020-05-19 12:06:17 +0100

    new addons added

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Web;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EmployeeeManagement.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public static ActionResult UploadFiles(IEnumerable<HttpPostedFileBase> files)
+        { 
+        }
+
+    }
+}
